ac6090bd0629ba12f291d105905c1522f7919046
Ricardo Torrena <riki.torrena@gmail.com>
2024-02-07 11:37:18 -0300

    Tarea de m3u3, necesito pasar a limpio el mensaje de error y enviado correctamente.

--- M3UTN/M3U3/Hamburgeseria/routes/index.js
+++ M3UTN/M3U3/Hamburgeseria/routes/index.js
@@ -9,6 +9,12 @@ router.get('/', function(req, res, next) {
 
 router.post('/', async (req, res, next) => {
 
+  if (!req.body.nombre || !req.body.apellido || !req.body.email || !req.body.telefono || !req.body.mensaje) {
+    return res.render('index', {
+      error: 'Todos los campos son obligatorios'
+    });
+  }
+
 var nombre = req.body.nombre;
 var apellido = req.body.apellido;
 var email = req.body.email;
@@ -18,7 +24,7 @@ var mensaje = req.body.mensaje;
 var obj = {
   to: 'riki.torrena@gmail.com',
   subject: 'Contacto desde la web',
-  html: nombre +" "+ apellido + " Se contacto a traves del formulario y quiere más información a este mail: " + email + ". <br>Además, hizo el siguiente comentario: " + mensaje + ".<br> Su telefono es " + telefono
+  html: nombre +" "+ apellido + " Se contacto a traves del formulario y quiere más información a este mail: " + email + ". <br>Además, hizo el siguiente comentario: " + mensaje + "<br> Su telefono es " + telefono
 } //cierra el var del obj
 
 var transporter = nodemailer.createTransport({
@@ -30,12 +36,17 @@ var transporter = nodemailer.createTransport({
   }
 }); //Cierra el transporter
 
+try {
   var info = await transporter.sendMail(obj);
-
-  res.render ('index', {
-    message: 'Mensaje enviado correctamente',
+  res.render('index', {
+    message: 'Mensaje enviado correctamente'
   });
-
+} catch (error) {
+  console.error("Error al enviar el correo:", error);
+  res.render('index', {
+    error: 'Hubo un error al enviar el mensaje, por favor inténtalo de nuevo más tarde'
+  });
+}
 
 }); //cierra la petision del post
 
